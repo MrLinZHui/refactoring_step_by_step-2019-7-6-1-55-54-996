b7f385ac37a74492170b61bf8c23401c9ec00560
MrLinZHui <742847492@qq.com>
2019-07-15 14:10:41 +0800

    should_return_TRUE_when_Driver_age_over_18

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,10 @@
 package com.tws.refactoring;
 
 public class Police {
+    private static int supAge = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+//        if(driver.age >= 18) return true;
+//        else return false;
+        return driver.getAge()>= Police.supAge?true:false;
     }
 }
---
+++ src/test/java/com/tws/refactoring/PoliceTest.java
@@ -0,0 +1,17 @@
+package com.tws.refactoring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PoliceTest {
+    @Test
+    public void should_return_TRUE_when_Driver_age_over_18(){
+        //given
+        Driver driver = new Driver(20);
+        Police police = new Police();
+        //when
+        boolean isstatus=police.checkDriver(driver);
+        //then
+        Assertions.assertEquals(true,isstatus);
+    }
+}
